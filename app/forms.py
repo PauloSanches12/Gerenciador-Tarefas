f059abe634ab6b58d44473094e3df8c28ec535a7
Paulo <paulo_sanches2012@hotmail.com>
2019-09-11 10:24:17 -0300

    alterações campo data

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Tarefa
+from django.forms import DateInput
+from .models import *
+
 class TarefaForm(forms.ModelForm):
     class Meta:
         model = Tarefa
         exclude = ('usuario', )
         fields = '__all__'
+        widgets = {
+            'data_expiracao': DateInput(attrs={'type': 'date'})
+        }
+
+
